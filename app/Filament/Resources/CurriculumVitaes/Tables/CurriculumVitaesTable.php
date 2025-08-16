f7825c86af27c833a5384a61a21b44418ed821a6
Jack Ellis <jackellis1504@gmail.com>
2025-08-16 21:15:20 +0100

    default sort
     M app/Filament/Resources/CurriculumVitaes/Tables/CurriculumVitaesTable.php

--- app/Filament/Resources/CurriculumVitaes/Tables/CurriculumVitaesTable.php
+++ app/Filament/Resources/CurriculumVitaes/Tables/CurriculumVitaesTable.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\CurriculumVitaes\Tables;
 
+use App\Models\CurriculumVitae;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
@@ -21,11 +22,12 @@ class CurriculumVitaesTable
                 TextColumn::make('created_at')->label('Created At')->sortable(),
                 TextColumn::make('updated_at')->label('Updated At')->sortable(),
             ])
+            ->defaultSort('created_at', 'desc')
             ->filters([
                 //
             ])
             ->recordActions([
-                EditAction::make(),
+                EditAction::make()->disabled(fn (CurriculumVitae $cv) => !is_null($cv->child))
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
